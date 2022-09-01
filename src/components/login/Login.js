48ff032fa2d5608e494d87b1717522aab363ab46
Pashimati <Pashimatii@gmail.com>
2022-09-01 15:48:43 +0300

    login and register

---
+++ src/components/login/Login.js
@@ -0,0 +1,12 @@
+import MyForm from "../my-form/My-form";
+
+const Login = () => {
+    return (
+        <div className="login-wrap">
+            <h2 className="login__title">Войдите пожалуйста</h2>
+            <MyForm/>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/register/Register.js
@@ -0,0 +1,12 @@
+import MyForm from "../my-form/My-form";
+
+const Register = () => {
+    return (
+        <div className="login-wrap">
+            <h2 className="login__title">Зарегистрируйтесь пожалуйста</h2>
+            <MyForm/>
+        </div>
+    );
+};
+
+export default Register;
