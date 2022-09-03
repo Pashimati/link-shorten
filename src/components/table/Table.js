c1805698e4d7d2d375bf1241f9527a62e2989311
Pashimati <Pashimatii@gmail.com>
2022-09-03 09:08:57 +0300

    fix table const

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -6,33 +6,34 @@ import _ from "lodash"
 
 const pageSize = 10;
 const Table = () => {
-    const [posts, setPosts] = useState();
-    const [paginatedPost, setPaginatedPost] = useState();
+    const [links, setLinks] = useState();
+    const [paginatedLink, setPaginatedLink] = useState();
     const [currentPage, setCurrentPage] = useState();
 
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/todos')
             .then(res => {
                 console.log(res.data)
-                setPosts(res.data)
-                setPaginatedPost(_(res.data).slice(0).take(pageSize).value())
+                setLinks(res.data)
+                setPaginatedLink(_(res.data).slice(0).take(pageSize).value())
             })
     }, [])
 
-    const pageCount = posts ? Math.ceil(posts.length/pageSize) : 0
+    const pageCount = links ? Math.ceil(links.length/pageSize) : 0
     if (pageCount === 1) return null;
     const pages = _.range(1, pageCount + 1)
 
     const pagination = (pageNo) => {
         setCurrentPage(pageNo)
         const startIndex = (pageNo - 1) * pageSize;
-        const paginatedPost = _(posts).slice(startIndex).take(pageSize).value();
-        setPaginatedPost(paginatedPost)
+        const paginatedLink = _(links).slice(startIndex).take(pageSize).value();
+        setPaginatedLink(paginatedLink)
     }
 
     return (
         <div className="table-container">
-            {!paginatedPost ? ("No data found"): (
+            <h1 className="table-title">Список ссылок</h1>
+            {!paginatedLink ? ("No data found"): (
                 <table className="table">
                     <thead>
                         <tr>
@@ -43,11 +44,11 @@ const Table = () => {
                     </thead>
                     <tbody>
                         {
-                            paginatedPost.map((post, index) => (
+                            paginatedLink.map((link, index) => (
                                 <tr key={index}>
-                                    <td>{post.id}</td>
-                                    <td>{post.userId}</td>
-                                    <td>{post.title}</td>
+                                    <td>{link.id}</td>
+                                    <td>{link.userId}</td>
+                                    <td>{link.title}</td>
                                 </tr>
                             ))
                         }
