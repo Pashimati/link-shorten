502559bb6f7ab98e7c8fcb2d0d1f7cad6960c377
Pashimati <Pashimatii@gmail.com>
2022-09-04 21:49:03 +0300

    configs

--- src/services/configs.js
+++ src/services/configs.js
@@ -1,4 +1,9 @@
 export const _apiBase = 'http://79.143.31.216'
-export const headers = {
-    "Content-Type": "multipart/form-data",
+
+export const _headers = (token) => {
+    return {
+        "Content-Type": "multipart/form-data",
+        'Authorization' : `Bearer ${token}`
+    }
 }
+
