2497404cfb458ce8083e0dec21b5ae728f671304
Pashimati <Pashimatii@gmail.com>
2022-09-04 21:49:33 +0300

    add short-link

--- src/components/add-short-link/Add-short-link.js
+++ src/components/add-short-link/Add-short-link.js
@@ -1,15 +1,24 @@
 import "./Add-short-link.scss"
 import ShortLinkInfo from "../short-link-info/Short-link-info";
 import {useState} from "react";
+import useLinkShortenService from "../../services/Link-shortenService";
 
 
 const AddShortLink = () => {
-    const [link, setLink] = useState()
-
-const data = true
+    const [link, setLink] = useState();
+    const [data, setData] = useState();
+    const {shortenLink} = useLinkShortenService();
 
     const shortLink = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+
+        shortenLink(link)
+            .then(res => {
+                setData(res)
+            })
+            .catch(e => {
+                setData(e.response.status)
+            })
     }
 
 
@@ -22,7 +31,7 @@ const data = true
             <h3 className="form-for-link__title">Cократите ссылку в 1 клик</h3>
             <form id='form' className="form-for-link">
                 <input
-                    onChange={onChangeHandler}
+                    onChange={(e) => onChangeHandler(e)}
                     className="form-for-link__input"
                     type="text"
                     placeholder="Вставьте ссылку для сокращения"
--- src/components/short-link-info/Short-link-info.js
+++ src/components/short-link-info/Short-link-info.js
@@ -2,29 +2,35 @@ import "./Short-link-info.scss"
 import {Link} from "react-router-dom";
 
 const ShortLinkInfo = ({data}) => {
-
-    if (!data) {
+    if (data === 422) {
         return <span className="error">Произошла ошибка. Попробуйте еще раз!</span>
     }
 
     return (
-        <div className="short-link-info-container">
-            <div className="row">
-                <div className="col-sm-6">
-                    <span className="long-url">https://hyperhost.ua/tools/ru/surli?gclid=Cj0KCQjw</span>
-                </div>
+        <>
+            {data && data !== 422 ? (
+                <div className="short-link-info-container">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <span className="long-url">{data.data.target.substring(1,70)}...</span>
+                        </div>
 
-                <div className="col-sm-3">
-                    <span className="short-url">http://surl.li/cwtlq</span>
-                </div>
-                <div className="col-sm-3">
-                    <div className="button-group">
-                        <button id="copy" className="btn-sl">Копировать</button>
-                        <Link className="sht-link" to="statistic-links">статистика</Link>
+                        <div className="col-sm-3">
+                            <span className="short-url">{data.data.short}</span>
+                        </div>
+                        <div className="col-sm-3">
+                            <div className="button-group">
+                                <button id="copy" className="btn-sl">Копировать</button>
+                                <Link className="sht-link" to="statistic-links">статистика</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            ) : null
+
+            }
+        </>
+
     );
 };
 
