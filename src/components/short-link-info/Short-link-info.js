25f45e33bde327639b85e8550fa48d277b065446
Pashimati <Pashimatii@gmail.com>
2022-09-05 21:58:13 +0300

    function copy text

--- src/components/short-link-info/Short-link-info.js
+++ src/components/short-link-info/Short-link-info.js
@@ -1,9 +1,22 @@
 import "./Short-link-info.scss"
 import {Link} from "react-router-dom";
+import toast from "react-simple-toasts";
 
 const ShortLinkInfo = ({data}) => {
     if (data === 422) {
         return <span className="error">Произошла ошибка. Попробуйте еще раз!</span>
+    }
+
+    function copyText() {
+        let copyText = document.querySelector(".short-url").textContent;
+        navigator.clipboard
+            .writeText(copyText)
+            .then(() => {
+                toast('Ссылка скопирована!')
+            })
+            .catch(() => {
+                toast('Ошибка при копировании')
+            });
     }
 
     return (
@@ -20,7 +33,7 @@ const ShortLinkInfo = ({data}) => {
                         </div>
                         <div className="col-sm-3">
                             <div className="button-group">
-                                <button id="copy" className="btn-sl">Копировать</button>
+                                <button id="copy" className="btn-sl" onClick={copyText}>Копировать</button>
                                 <Link className="sht-link" to="statistic-links">статистика</Link>
                             </div>
                         </div>
