511564546aede3af24541e555914fac8d696034f
Pashimati <Pashimatii@gmail.com>
2022-09-05 16:23:36 +0300

    sort table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,81 +1,98 @@
 import {useEffect, useState} from "react";
 import './Table.scss'
-import axios from "axios";
-import _ from "lodash"
+import useLinkShortenService from "../../services/Link-shortenService";
+import MySelect from "../my-select/My-select";
 
 
-const pageSize = 10;
 const Table = () => {
     const [links, setLinks] = useState();
-    const [paginatedLink, setPaginatedLink] = useState();
-    const [currentPage, setCurrentPage] = useState();
+    const [selectedSort, setSelectedSort] = useState('');
+    const { getStatistics } = useLinkShortenService();
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/todos')
+        getStatistics()
             .then(res => {
-                console.log(res.data)
                 setLinks(res.data)
-                setPaginatedLink(_(res.data).slice(0).take(pageSize).value())
             })
     }, [])
 
-    const pageCount = links ? Math.ceil(links.length/pageSize) : 0
-    if (pageCount === 1) return null;
-    const pages = _.range(1, pageCount + 1)
-
-    const pagination = (pageNo) => {
-        setCurrentPage(pageNo)
-        const startIndex = (pageNo - 1) * pageSize;
-        const paginatedLink = _(links).slice(startIndex).take(pageSize).value();
-        setPaginatedLink(paginatedLink)
+    function compareNumbers(a, b) {
+        return a - b;
     }
 
+    const sortLinks = (sort) => {
+        setSelectedSort(sort)
+        if(sort === 'id' || sort === 'counter'){
+            return  setLinks([...links].sort((a, b) => a[sort] - b[sort]));
+        }
+        else{
+            return  setLinks([...links].sort((a, b) =>  a[sort].localeCompare(b[sort])))
+        }
+    }
     return (
         <div className="table-container">
             <h1 className="table-title">Список ссылок</h1>
-            {!paginatedLink ? ("No data found"): (
+            <MySelect
+                value={selectedSort}
+                onChange={sortLinks}
+                defaultValue="Сортировка"
+                options={[
+                    {value: 'target', name: 'По названию'},
+                    {value: 'counter', name: 'По количеству кликов'},
+                    {value: 'id', name: 'По уникальному индификатору'},
+                ]}
+            />
+
+            {!links
+                ? <h3 className='table-notfound'>Список пуст</h3>
+                : (
                 <table className="table">
                     <thead>
                         <tr>
-                           <th>Id</th>
-                           <th>USER Id</th>
-                           <th>Title</th>
+                           <th>Позиция</th>
+                           <th>Уникальный идентификатор</th>
+                           <th>Стандартная ссылка</th>
+                           <th>Короткая ссылка</th>
+                           <th>Количество переходов</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            paginatedLink.map((link, index) => (
-                                <tr key={index}>
+                            links.map((link, index) => (
+                                <tr key={index + 1}>
+                                    <td>{index + 1}</td>
                                     <td>{link.id}</td>
-                                    <td>{link.userId}</td>
-                                    <td>{link.title}</td>
+                                    <td>{link.target.substring(0,28)}...</td>
+                                    <td>{link.short}</td>
+                                    <td>{link.counter}</td>
                                 </tr>
                             ))
                         }
                     </tbody>
                 </table>
             )}
-            <nav className="nav">
-                <ul className="pagination">
-                    {
-                        pages.map((page) => (
-                            <li
-                                className={page === currentPage ? "page-item active" : "page-item"}
-                            >
-                                <p
-                                    className="page-link"
-                                    onClick={() => pagination(page)}
-                                >
-                                    {page}
-                                </p>
-
-                            </li>
-                        ))
-                    }
-                </ul>
-            </nav>
         </div>
     );
 };
 
 export default Table;
+
+// <nav className="nav">
+//     <ul className="pagination">
+//         {
+//             pages.map((page) => (
+//                 <li
+//                     className={page === currentPage ? "page-item active" : "page-item"}
+//                 >
+//                     <p
+//                         className="page-link"
+//                         onClick={() => pagination(page)}
+//                     >
+//                         {page}
+//                     </p>
+//
+//                 </li>
+//             ))
+//         }
+//     </ul>
+// </nav>
